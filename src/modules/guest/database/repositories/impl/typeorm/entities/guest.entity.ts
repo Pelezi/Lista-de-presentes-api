1a8bcf105546b2df6dbc6bda3e159afe51abe5bd
Alessandro <abcsandro@hotmail.com>
2025-01-18 15:01:06 -0300

    .

--- src/modules/guest/database/repositories/impl/typeorm/entities/guest.entity.ts
+++ src/modules/guest/database/repositories/impl/typeorm/entities/guest.entity.ts
@@ -4,40 +4,6 @@ import { Entity, Column, PrimaryGeneratedColumn, ManyToMany } from "typeorm";
 
 @Entity()
 export class Guest {
-    
-    // @PrimaryGeneratedColumn("uuid")
-    // id: string;
-
-    // @Column()
-    // name: string;
-
-    // @Column()
-    // photoUrl: string;
-
-    // @Column()
-    // quantity: number;
-
-    // @Column()
-    // description: string;
-    
-    // @ManyToMany(() => Guest, guest => guest.gifts)
-    // guests: Guest[];
-
-    // gift
-    // 	id
-    // 	name
-    // 	photoUrl
-    // 	quantity
-    // 	description
-
-    // guests
-    // 	id
-    // 	Nome
-    // 	phone
-
-    // presentes_convidadas
-    // 	giftId
-    // guestId
 
     @PrimaryGeneratedColumn("uuid")
     id: string;
